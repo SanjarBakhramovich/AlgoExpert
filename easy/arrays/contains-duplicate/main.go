80547d83641f61deddc31d0ad5b70736d7dda58a
Sanjar <sanjarbakhramovich@gmail.com>
2023-11-03 12:23:17 +0500

    add two solutions, valid-anagram and contains-duplicate

---
+++ easy/arrays/contains-duplicate/main.go
@@ -0,0 +1,12 @@
+package main
+
+func containsDuplicate(nums []int) bool {
+	hashSet := make(map[int]bool)
+	for _, num := range nums {
+		if hashSet[num] {
+			return true
+		}
+		hashSet[num] = true
+	}
+	return false
+}
